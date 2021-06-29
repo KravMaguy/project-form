6879d30e65a2a07fd2570a44e6fcceece28c7134
krav maguy <flex4lease@gmail.com>
2021-06-29 17:07:00 -0500

    basic form setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+function List() {
+  const [numberList, setNumberList] = useState([]);
+  const [input, setInput] = useState(0);
 
-function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const number = {
+      input,
+      id: new Date().getTime().toString(),
+      ordered: false,
+    };
+    setNumberList([...numberList, number]);
+    setInput("");
+    console.log(numberList);
+  };
+  const handleChange = (e) => {
+    const val = e.target.value;
+    setInput(val);
+  };
+  const handleClick = (id) => {
+    console.log("clicked: ", id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input onChange={handleChange} type="number" value={input} />
+        </div>
+        <button type="submit" value="submit">
+          add{" "}
+        </button>
+      </form>
+      {numberList.map((number) => {
+        return (
+          <div
+            onClick={() => handleClick(number.id)}
+            className="item"
+            key={number.id}
+          >
+            <h4>Number: {number.input}</h4>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <div className="container">
+      <List />
+    </div>
+  );
+}
