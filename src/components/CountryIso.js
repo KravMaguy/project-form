a3402296fa8b3e068e9330c8a2091b57f31bd824
krav maguy <flex4lease@gmail.com>
2021-07-01 17:59:13 -0500

    country Iso completed

--- src/components/CountryIso.js
+++ src/components/CountryIso.js
@@ -1,24 +1,34 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const fetchCountries = () => {
-  const url = "https://restcountries.eu/rest/v2/all";
-  return axios.get(url).then(({ data }) => console.log(data));
+const fetchCountries = (url) => {
+  return axios
+    .get(url)
+    .then(({ data }) => data)
+    .catch((err) => console.error(err));
 };
+
 function CountryIso() {
-  const [country, setCountry] = useState([]);
+  const url = "https://restcountries.eu/rest/v2/all";
+  const [countries, setCountries] = useState([]);
   useEffect(() => {
     console.log("get the api and put in state");
-    fetchCountries();
+    fetchCountries(url).then((countriesInfo) => setCountries(countriesInfo));
   }, []);
+  console.log(countries);
   return (
     <>
       <label for="cars">Choose a car:</label>
-      <select name="cars" id="cars">
+      {/* <select name="cars" id="cars">
         <option value="volvo">Volvo</option>
         <option value="saab">Saab</option>
         <option value="mercedes">Mercedes</option>
         <option value="audi">Audi</option>
+      </select> */}
+      <select>
+        {countries.map((option) => (
+          <option value={option.alpha2Code}>{option.name}</option>
+        ))}
       </select>
     </>
   );
