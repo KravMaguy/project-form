b12f278fce37420488d5728f6e5a4028d82ebb26
krav maguy <flex4lease@gmail.com>
2021-07-01 19:26:53 -0500

    css grid 4x4 large screen, single col on mobile

--- src/components/ButtonGrid.js
+++ src/components/ButtonGrid.js
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 
-function Button() {
-  const [color, setColor] = useState("#7CFC00");
+function ButtonGrid() {
   //array of ids
   const n = 16;
   const [clickedButton, setClickedButton] = useState([]);
-  // const [checked, setChecked]=useState()
   const handleClick = (id) => {
-    console.log("i was clicked: ", id);
     if (clickedButton.includes(id)) {
       const arr = clickedButton.filter((x) => x !== id);
       setClickedButton(arr);
@@ -16,21 +13,23 @@ function Button() {
     }
   };
 
-  return [...Array(n)].map((e, id) => (
-    <button
-      className={clickedButton.includes(id) ? "greenbutton" : "whitebutton"}
-      onClick={() => handleClick(id)}
-      key={id}
-    >
-      Change color
-    </button>
-  ));
-}
-export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <Button className="GridStyle" />
+    <div className="button-grid">
+      {[...Array(n)].map((e, id) => (
+        <button
+          className={
+            clickedButton.includes(id)
+              ? "greenbutton button-component"
+              : "whitebutton button-component"
+          }
+          onClick={() => handleClick(id)}
+          key={id}
+        >
+          Change color
+        </button>
+      ))}
     </div>
   );
 }
+
+export default ButtonGrid;
