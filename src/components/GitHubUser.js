587e288c99dd11bad8e53adc7ba84fea7079d828
krav maguy <flex4lease@gmail.com>
2021-07-18 06:06:59 -0500

    input select

--- src/components/GitHubUser.js
+++ src/components/GitHubUser.js
@@ -3,28 +3,51 @@ import axios from "axios";
 
 function GitHubUser() {
   const [input, setInput] = useState("");
+  const [gitUsers, setGitUsers] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("submitted");
-    axios.get(`https://api.github.com/users/${input}`).then(({ data }) => {
-      console.log("data", data);
-      setInput("");
-    });
+    axios
+      .get(`https://api.github.com/search/users?q=${input}`)
+      .then(({ data }) => {
+        const { items } = data;
+        setInput("");
+        setGitUsers(items);
+      })
+      .catch((e) => console.log("err", e));
   };
 
   const handleChange = (e) => {
     setInput(e.target.value);
+    console.log("handleChange ", input);
   };
 
   return (
     <form className="form" onSubmit={handleSubmit}>
-      <div className="button-in">
-        <input value={input} onChange={handleChange} type="text" />
-        <button type="submit" value="submit">
-          Search{" "}
-        </button>
-      </div>
+      {gitUsers.length < 1 ? (
+        <input
+          list="opts"
+          name="user"
+          value={input}
+          onChange={handleChange}
+          id="search-user"
+        />
+      ) : (
+        <select
+          onChange={handleChange}
+          name="user-select"
+          id="user-select"
+          className="country-select"
+        >
+          {gitUsers.map((option) => (
+            <option key={option.id} value={option.login}>
+              {option.login}
+            </option>
+          ))}
+        </select>
+      )}
+      <input type="submit" value="search" />
     </form>
   );
 }
