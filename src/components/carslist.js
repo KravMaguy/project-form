eff5a7876bc1f12fb0f69372fa548ed3b2252318
krav maguy <flex4lease@gmail.com>
2021-08-28 15:15:44 -0500

    dynamically generated select

--- src/components/carslist.js
+++ src/components/carslist.js
@@ -1,33 +1,36 @@
 import { useState, useEffect } from "react";
 import cardata from "./cardata";
 
-
 // https://assets.codepen.io/19636/deere-challenge.jpg
 // https://codepen.io/kravmaguy/project/editor/DQQygx
 
 const CarsList = () => {
-  const [carsData, setCarData] = useState(cardata);
-  const [checkMazda, setcheckMazda] = useState(true);
-  const [checkSubaru, setCheckSubaru] = useState(false);
+  const [models, setModals] = useState([]);
+  const [checkedState, setCheckedState] = useState([]);
+  const [carsData, setCarData] = useState([]);
   const [selectedCar, setSelectedCar] = useState(null);
   const [selectedModels, setSelectedModels] = useState(["Mazda"]);
 
-  const handleSelectMazda = (e) => {
-    console.log(e.target.value);
-    const value = e.target.value;
-    setcheckMazda(!checkMazda);
-    if (selectedModels.includes(value)) {
-      const arr = selectedModels.filter((x) => x !== value);
-      setSelectedModels(arr);
-    } else {
-      setSelectedModels([...selectedModels, value]);
-    }
-  };
+  useEffect(() => {
+    const carModals = [];
+    cardata.map((car, id) => {
+      car.id = id;
+      return carModals.includes(car.make) ? null : carModals.push(car.make);
+    });
+    setCarData(cardata);
+    setModals(carModals);
+    const checkedState = new Array(carModals.length).fill(false);
+    checkedState[0] = true;
+    setCheckedState(checkedState);
+  }, []);
 
-  const handleSelectSubaru = (e) => {
-    console.log(e.target.value);
+  const handleChecked = (e, pos) => {
     const value = e.target.value;
-    setCheckSubaru(!checkSubaru);
+    const updatedChecked = checkedState.map((item, idx) =>
+      idx === pos ? !item : item
+    );
+    setCheckedState(updatedChecked);
+    console.log(updatedChecked);
     if (selectedModels.includes(value)) {
       const arr = selectedModels.filter((x) => x !== value);
       setSelectedModels(arr);
@@ -37,69 +40,103 @@ const CarsList = () => {
   };
 
   const selectCar = (id) => {
-    console.log(id);
     setSelectedCar(id);
   };
 
-  // useEffect(() => {
-  //   console.log(cardata, "carData");
-  // }, []);
-
-  console.log(selectedModels, "selectedModels");
   const displayedCars = carsData.filter((car) =>
     selectedModels.includes(car.make)
   );
-  console.log(displayedCars, "displayedCars");
 
   return (
     <div className="border-2 border-gray-700 p-3">
+      {" "}
+           
       <div className="row g-4">
+        {" "}
+               
         <div className="col-md-3">
+          {" "}
+                   
           <div className="border-2 border-gray-700 p-3">
-            <h3 className="text-xl">Filters</h3>
-            <label>
-              <input
-                type="checkbox"
-                checked={checkMazda}
-                onChange={(e) => handleSelectMazda(e)}
-                value={"Mazda"}
-              />
-              Mazda
-            </label>
-            <label>
-              <input
-                type="checkbox"
-                checked={checkSubaru}
-                onChange={(e) => handleSelectSubaru(e)}
-                value={"Subaru"}
-              />
-              Subaru
-            </label>
-          </div>
-        </div>
-
+            <h1 className="text-xl">Filters</h1>           
+            {models.length > 0
+              ? models.map((model, idx) => (
+                  <label key={model}>
+                    {" "}
+                                       
+                    <input
+                      type="checkbox"
+                      checked={checkedState[idx]}
+                      onChange={(e) => handleChecked(e, idx)}
+                      value={model}
+                    />
+                    {model}                 
+                  </label>
+                ))
+              : null}{" "}
+                     
+          </div>{" "}
+                 
+        </div>{" "}
+               
         <div className="col-md-4">
-          {displayedCars.map((car, idx) => (
+          {" "}
+                   
+          {displayedCars.map((car) => (
             <div
-              key={idx}
-              onClick={() => selectCar(idx)}
-              className="border-2 border-gray-700 p-3"
+              key={car.id}
+              onClick={() => selectCar(car.id)}
+              className={
+                car.id === selectedCar
+                  ? "border-2 border-gray-700 p-3 bg-gray-600 text-white"
+                  : "border-2 border-gray-700 p-3"
+              }
             >
-              {car.year} {car.make} {car.model}
+              {car.year} {car.make} {car.model}           
             </div>
-          ))}
+          ))}{" "}
+                 
         </div>
-
+               
         <div className="col-md-5">
+          {" "}
+                   
           <div className="border-2 border-gray-700 p-3">
+            {" "}
+                       
             {!selectedCar && selectedCar !== 0 ? (
               <div>Select a car to display details</div>
             ) : (
-              <div>({carsData[selectedCar].year}) {carsData[selectedCar].make}, {carsData[selectedCar].model}</div>
-            )}
-          </div>
-        </div>
-      </div>
+              <div className="grid grid-cols-2 ml-10">
+                <div>
+                  <h2 className="text-xl text-left">
+                    ({carsData[selectedCar].year}) {carsData[selectedCar].make},{" "}
+                    {carsData[selectedCar].model}
+                  </h2>
+                  <div className="text-left">
+                    make : {carsData[selectedCar].make} <br />
+                    model : {carsData[selectedCar].model} <br />
+                    year : {carsData[selectedCar].year} <br />
+                    price : {carsData[selectedCar].price} <br />
+                    location : {carsData[selectedCar].location} <br />
+                  </div>
+                </div>
+                <div className="m-10">
+                  <img
+                    alt="placeholder"
+                    className="placeholder"
+                    src="./placeholder.png"
+                  />
+                </div>
+              </div>
+            )}{" "}
+                     
+          </div>{" "}
+                 
+        </div>{" "}
+             
+      </div>{" "}
+         
     </div>
   );
 };
